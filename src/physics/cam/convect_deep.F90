34c6921192a713d92d76783ec506277265eae7f9
jatkinson1000 <jwa34@cam.ac.uk>
2024-06-18 03:50:23 -0600

    Remove use of YOG as deep_scheme option in convect_deep.

--- src/physics/cam/convect_deep.F90
+++ src/physics/cam/convect_deep.F90
@@ -92,9 +92,6 @@ subroutine convect_deep_register
   case('ZM') !    Zhang-McFarlane (default)
      call zm_conv_register
 
-  case('YOG') !    Yuval-O'Gorman
-     call zm_conv_register
-
   case('off', 'UNICON') ! Off needs to setup the following fields
    call pbuf_add_field('ICWMRDP',    'physpkg',dtype_r8,(/pcols,pver/),icwmrdp_idx)
    call pbuf_add_field('RPRDDP',     'physpkg',dtype_r8,(/pcols,pver/),rprddp_idx)
@@ -146,9 +143,6 @@ subroutine convect_deep_init(pref_edge)
   case('SPCAM')
      if (masterproc) write(iulog,*)'convect_deep: deep convection done by SPCAM'
      return
-  case('YOG')
-     if (masterproc) write(iulog,*)'convect_deep: deep convection done by YOG'
-     call zm_conv_init(pref_edge)
   case default
      if (masterproc) write(iulog,*)'WARNING: convect_deep: no deep convection scheme. May fail.'
   end select
@@ -279,17 +273,6 @@ subroutine convect_deep_tend( &
           jctop, jcbot , &
           state   ,ptend   ,landfrac, pbuf)
 
-  case('YOG') !    Yuval-O'Gorman (running ZM for now)
-     call pbuf_get_field(pbuf, pblh_idx,  pblh)
-     call pbuf_get_field(pbuf, tpert_idx, tpert)
-
-     call zm_conv_tend( pblh    ,mcon    ,cme     , &
-          tpert   ,pflx    ,zdu      , &
-          rliq    ,rice    , &
-          ztodt   , &
-          jctop, jcbot , &
-          state   ,ptend   ,landfrac, pbuf)
-
   end select
 
   ! If we added temperature tendency to pbuf, set it now.
@@ -324,10 +307,7 @@ subroutine convect_deep_tend_2( state,  ptend,  ztodt, pbuf)
 
    if ( deep_scheme .eq. 'ZM' ) then  ! Zhang-McFarlane
       call zm_conv_tend_2( state,   ptend,  ztodt,  pbuf) 
-   else if ( deep_scheme .eq. 'YOG' ) then  ! Yuval-O'Gorman
-      ! TODO Do nothing extra for now as seems to be a separate scheme
-      call zm_conv_tend_2( state,   ptend,  ztodt,  pbuf)
-    else
+   else
       call physics_ptend_init(ptend, state%psetcols, 'convect_deep')
    end if
 
