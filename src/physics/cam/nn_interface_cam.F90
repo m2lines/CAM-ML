04e086dfb039895e90c5f18255bcf4c211cd8585
MarionBWeinzierl <mw925@cam.ac.uk>
2024-12-09 06:42:58 -0700

    implementation of yog energy check

--- src/physics/cam/nn_interface_cam.F90
+++ src/physics/cam/nn_interface_cam.F90
@@ -13,6 +13,8 @@ use SAM_consts_mod, only: nrf, ggr, cp, tbgmax, tbgmin, tprmax, tprmin, &
                           fac_cond, fac_sub, fac_fus, &
                           a_bg, a_pr, an, bn, ap, bp, &
                           omegan, check
+use physconst, only: gravit
+use ppgrid, only: pcols, pver, begchunk, endchunk
 
 implicit none
 private
@@ -20,7 +22,7 @@ private
 
 !---------------------------------------------------------------------
 ! public interfaces
-public  nn_convection_flux_CAM, &
+public  yog_conservation_check, nn_convection_flux_CAM, &
         nn_convection_flux_CAM_init, nn_convection_flux_CAM_finalize
 
 ! Make these routines public for purposes of testing,
@@ -71,6 +73,35 @@ contains
 
     end subroutine nn_convection_flux_CAM_init
 
+    subroutine yog_conservation_check(cpair_v_loc, pdel, t, qv, qc, qi, se, & 
+            sw, lchnk, ncol)
+
+        real(8), intent(in) :: cpair_v_loc(:,:,:) ! heat capacity
+        real(8), intent(in) :: pdel
+        real(8), intent(in) :: t(:,:)
+        real(8), intent(in) :: qv(:,:), qc(:,:), qi(:, :)
+        real(8), intent(out) :: se(:)       ! sum energy
+        real(8), intent(out) :: sw(:)       ! sum water
+        real(8) wv(:), wl(:), wi(:)
+
+        integer, intent(in) :: lchnk                 ! chunk identifier
+        integer, intent(in) :: ncol                  ! number of atmospheric columns
+        integer  i,k                                 ! column, level indices
+
+        do k = 1, pver
+          do i = 1, ncol
+            se(i) = se(i) + t(i,k)*cpairv_loc(i,k,lchnk)*pdel/gravit
+            wv(i) = wv(i) + qv(i,k)                    *pdel/gravit
+            wl(i) = wl(i) + qc(i,k)                    *pdel/gravit
+            wi(i) = wi(i) + qi(i,k)                    *pdel/gravit
+            sw(i) = wv(i) + wl(i) + wi(i)
+          end do
+        end do
+        
+        write(iulog, *), "Energy sum: ", se
+        write(iulog, *), "Water sum: ", sw
+
+    end subroutine yog_conservation_check
 
     subroutine nn_convection_flux_CAM(pres_cam, pres_int_cam, pres_sfc_cam, &
                                       tabs_cam, qv_cam, qc_cam, qi_cam, &
@@ -126,6 +157,11 @@ contains
         precsfc(:)=0.
 
         !-----------------------------------------------------
+
+        yog_conservation_check(cp_cam, pdel, tabs_cam, qv_cam, qc_cam, qi_cam, se, &
+            sw, begchnk, nx, nz)
+
+        !-----------------------------------------------------
         
         ! Interpolate CAM variables to the SAM pressure levels
         ! TODO Interpolate all variables in one call
@@ -144,7 +180,9 @@ contains
         end where
         call extrapolate_to_surface(pres_cam, qv_cam, pres_sfc_cam, qv_surf)
         where (qv_surf>=0)
-            qv_surf = qv_surf
+            qv_surf = qv_sur(cpair_v_loc, pdel, gravit, t, qv, qc, qi, se, &
+            sw, lchnk, ncol, pver)
+
         elsewhere
             qv_surf = 0
         end where
