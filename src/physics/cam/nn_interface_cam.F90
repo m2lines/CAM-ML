0f7ab90b465bbb78b100d7f70bf96430d021bf94
MarionBWeinzierl <mw925@cam.ac.uk>
2024-12-10 06:01:58 -0700

    try other pressure array

--- src/physics/cam/nn_interface_cam.F90
+++ src/physics/cam/nn_interface_cam.F90
@@ -163,7 +163,7 @@ contains
 
         !-----------------------------------------------------
 
-        call yog_conservation_check(pres_sfc_cam, tabs_cam, qv_cam, qc_cam, qi_cam, nx)
+        call yog_conservation_check(pres_cam, tabs_cam, qv_cam, qc_cam, qi_cam, nx)
 
         !-----------------------------------------------------
         
