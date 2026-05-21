e39b69e1ccb3589ab3d64f9f22ed54e14ab30c4a
Mikolaj Adam Kowalski <32641577+Mikolaj-A-Kowalski@users.noreply.github.com>
2026-05-21 13:30:00 +0100

    Add ML-enabled CLUBB to CAM (#59)
    * point to an ML-enabled m2lines CLUBB fork
    
    * Update CAM's CLUBB interface
    
    Add the changes applied in the `clubb_driver` needed to get NN working.
    
    * add namelist parameters needed for CLUBB with ML
    
    * set up C14_ML during initialization
    
    * Update the CLUBB commit hash in .gitmodules and provide extra context
    
    * Revert clubb_c14_ml_net_filepath to 100 characters
    
    It is temporary and will be reverted when the CLUBB is also updated to
    accept 256 character paths.
    
    ---------
    
    Co-authored-by: David Kamm <d-kamm@web.de>

--- src/physics/cam/clubb_intr.F90
+++ src/physics/cam/clubb_intr.F90
@@ -31,6 +31,8 @@ module clubb_intr
   use ref_pres,            only: top_lev => trop_cloud_top_lev
   use scamMOD,             only: single_column, scm_clubb_iop_name, scm_cambfb_mode
 
+  use advance_xp2_xpyp_module, only: &
+      setup_C14_ML_xp2_xpyp   !------------------------------------------ Procedure(s)
 #ifdef CLUBB_SGS
   use clubb_api_module,    only: pdf_parameter, implicit_coefs_terms, &
                                  clubb_config_flags_type, grid, stats, &
@@ -755,7 +757,7 @@ end subroutine clubb_init_cnst
     use units,           only: getunit, freeunit
     use cam_abortutils,  only: endrun
     use spmd_utils,      only: mpicom, mstrid=>masterprocid, mpi_logical, mpi_real8, &
-                               mpi_integer
+                               mpi_integer, mpi_character
     use clubb_mf,        only: clubb_mf_readnl
 
     use clubb_api_module, only: &
@@ -773,6 +775,9 @@ end subroutine clubb_init_cnst
     logical :: clubb_cloudtop_cooling = .false., clubb_rainevap_turb = .false.
 
     integer :: iunit, read_status, ierr
+
+    logical :: clubb_l_c14_ml
+    character(len=100) :: clubb_c14_ml_net_filepath
 
     namelist /clubb_his_nl/ clubb_history, clubb_rad_history
     namelist /clubbpbl_diff_nl/ clubb_cloudtop_cooling, clubb_rainevap_turb, &
@@ -899,7 +904,9 @@ end subroutine clubb_init_cnst
          clubb_up2_sfc_coef, &
          clubb_wpxp_L_thresh, &
          clubb_wpxp_Ri_exp, &
-         clubb_z_displace
+         clubb_z_displace, &
+         clubb_l_c14_ml, &
+         clubb_c14_ml_net_filepath
 
     !----- Begin Code -----
 
@@ -978,7 +985,9 @@ end subroutine clubb_init_cnst
                                              clubb_l_mono_flux_lim_spikefix, &  ! Out
                                              clubb_l_host_applies_sfc_fluxes, & ! Out
                                              clubb_l_wp2_fill_holes_tke, & ! Out
-                                             clubb_l_add_dycore_grid ) ! Out
+                                             clubb_l_add_dycore_grid, & ! Out
+                                             clubb_l_c14_ml, & ! Out
+                                             clubb_c14_ml_net_filepath) ! Out
 
     !  Call CLUBB+MF namelist
     call clubb_mf_readnl(nlfile)
@@ -1429,7 +1438,9 @@ end subroutine clubb_init_cnst
                                                  clubb_l_mono_flux_lim_spikefix, &          ! In                      
                                                  clubb_l_host_applies_sfc_fluxes, &         ! In                      
                                                  clubb_l_wp2_fill_holes_tke, &              ! In                  
-                                                 clubb_l_add_dycore_grid, &                 ! In              
+                                                 clubb_l_add_dycore_grid, & 
+                                                 clubb_l_c14_ml, &
+                                                 clubb_c14_ml_net_filepath, &                ! In              
                                                  clubb_config_flags )                       ! Out
 
 #endif
@@ -2042,6 +2053,10 @@ end subroutine clubb_init_cnst
 
     ! The following is physpkg, so it needs to be initialized every time
     call pbuf_set_field(pbuf_ini, fice_idx,    0.0_r8)
+
+    if ( clubb_config_flags%l_c14_ml ) then
+      call setup_C14_ML_xp2_xpyp( clubb_config_flags%c14_ml_net_filepath )
+    end if
 
     ! --------------- !
     ! End             !
